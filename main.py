445a2d7c4b6594b626e4458d01f31ce86b38f49a
LulzSadPanda <lulzsadpanda@voodoopizzeria.com>
2025-01-14 19:25:15 +0000

    fixed python code

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def do_something(event):
     input_text = document.querySelector("#something")
     inputz = input_text.value
     output_div = document.querySelector("#output")
-    output_div.innerText = output_div[::-1]
+    output_div.innerText = inputz[::-1]
